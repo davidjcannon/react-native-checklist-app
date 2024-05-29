ad9b9ad4366e01aae5d94f67fce1f442231d8c57
David Cannon <54728106+davidjcannon@users.noreply.github.com>
2024-05-28 20:07:04 -0400

    Add "Add Checklist Item" Button
    Added new button and optimised some old code

--- components/TodoList.js
+++ components/TodoList.js
@@ -6,6 +6,7 @@ import {
   View,
   TouchableOpacity,
   FlatList,
+  KeyboardAvoidingView,
 } from 'react-native';
 
 export default class TodoList extends React.Component {
@@ -16,18 +17,22 @@ export default class TodoList extends React.Component {
   };
 
   renderTodo = (todo) => {
-    const list = this.props.list
+    const list = this.props.list;
     return (
       <TouchableOpacity
-        style={[styles.container, { backgroundColor: `${list.color}80` }]}>
+        style={[styles.container, { backgroundColor: `${list.color}40` }]}>
         <TouchableOpacity>
-            {list.completed ? (
-              <Feather name="check-square" style={styles.icon} />
-            ) : (
-              <Feather name="square" style={styles.icon} />
-            )}
-          </TouchableOpacity>
-        <Text style={styles.itemText} numberOfLines={1}>{todo.title}</Text>
+          <Feather
+            name={todo.completed ? 'check-square' : 'square'}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        <Text
+        style={[styles.itemText,
+            {textDecorationLine: todo.completed ? "line-through" : "none"},
+            {opacity: todo.completed ? 0.5 : 1}]} numberOfLines={1}>
+          {todo.title}
+        </Text>
       </TouchableOpacity>
     );
   };
@@ -36,25 +41,33 @@ export default class TodoList extends React.Component {
     const list = this.props.list;
     return (
       <View>
+        {/* Open/Close Category Button */}
         <TouchableOpacity
           style={[styles.container, { backgroundColor: list.color }]}>
+          {/* Check/Uncheck Button */}
           <TouchableOpacity>
-            {list.completed ? (
-              <Feather name="check-square" style={styles.icon} />
-            ) : (
-              <Feather name="square" style={styles.icon} />
-            )}
+            <Feather
+              name={list.completed ? 'check-square' : 'square'}
+              style={styles.icon}
+            />
           </TouchableOpacity>
           <Text style={styles.categoryText} numberOfLines={1}>
             {list.name}
           </Text>
         </TouchableOpacity>
         <View>
+          {/* Todo List Items */}
           <FlatList
             data={this.state.todos}
             renderItem={({ item }) => this.renderTodo(item)}
             keyExtractor={(item) => item.title}
           />
+          {/* Add Button */}
+          <TouchableOpacity
+            style={[styles.addItem, { backgroundColor: `${list.color}20` }]}>
+            <Feather name="plus" size={22} color="black" />
+            <Text style={styles.addItemText}>Add Checklist Item</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -66,24 +79,37 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     height: 40,
-    marginBottom: 8,
+    marginBottom: 5,
     borderRadius: 10,
   },
   categoryText: {
-    paddingLeft: 8,
+    paddingLeft: 6,
     fontSize: 24,
     fontWeight: '600',
     paddingBottom: 4,
   },
   itemText: {
-    paddingLeft: 8,
-    fontSize: 24,
+    paddingLeft: 6,
+    fontSize: 16,
     paddingBottom: 4,
   },
   icon: {
     fontSize: 26,
     color: 'black',
-    paddingLeft: 8,
+    paddingLeft: 6,
     opacity: 1,
+  },
+  addItem: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginBottom: 8,
+  },
+  addItemText: {
+    opacity: 0.8,
+    fontWeight: '600',
+    paddingBottom: 2,
+    paddingLeft: 2,
   },
 });
